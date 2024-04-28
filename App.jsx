c54c2e9cc951f6dd85a3efe1322dcf42fd61866c
anupam416 <168280410+anupam416@users.noreply.github.com>
2024-04-28 12:51:52 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <address id='address'>
+     Savitribai Phule Pune University <br></br>
+     2.0 km · Ganeshkhind Rd · 020 7153 3633
+     </address>
+     <div className='responsive-map'>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7564.677866860364!2d73.81444121119013!3d18.55875230082702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf0eed1beca7%3A0xf07ab6b613c2af51!2sSavitribai%20Phule%20Pune%20University!5e0!3m2!1sen!2sin!4v1714287361907!5m2!1sen!2sin" width="600" height="450"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+     </div>
+      </>
+  )
+}
+
+export default App
